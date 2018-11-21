da3650ec9aa0eb727102412eb3445bd3d7b19b36
Lucidastar <lucidahxx@gmail.com>
2018-11-21 15:43:57 +0800

    添加了测试

--- app/src/main/java/com/lucidastar/testcommutils/MainActivity.java
+++ app/src/main/java/com/lucidastar/testcommutils/MainActivity.java
@@ -3,11 +3,14 @@ package com.lucidastar.testcommutils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mine.lucidastarutils.log.KLog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        KLog.i("测试一下");
     }
 }
